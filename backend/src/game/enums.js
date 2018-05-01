3dd8746342dc8916c44b1b429a4d33b7da5e839c
Piotr Krawiranda <piotrkrawiranda@gmail.com>
2018-05-01 12:22:00 +0200

    Clean up js files

--- backend/src/game/enums.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MoveDirectionsArray = ["LEFT" /* LEFT */, "RIGHT" /* RIGHT */, "UP" /* UP */, "DOWN" /* DOWN */, "NO_MOVE" /* NO_MOVE */];
